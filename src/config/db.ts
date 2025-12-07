4a1656bb50380b10d83b17150342efa9243ed634
SongMingyeom <thdalsrua12@snu.ac.kr>
2025-12-07 20:10:08 +0900

    [Chore] Save local changes before pull

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,25 @@
-import mongoose from "mongoose";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
-    console.log("✅ MongoDB Connected");
+    await pool.connect();
+    console.log("🚀 Connected to PostgreSQL RDS");
   } catch (err) {
-    console.error("❌ MongoDB Connection Error:", err);
+    console.error("❌ PostgreSQL Connection Error", err);
     process.exit(1);
   }
 };
